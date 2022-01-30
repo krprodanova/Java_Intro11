3895e0d796df76ad08c9327153720e42b592d154
krprodanova <kremena.prodanov@abv.bg>
2022-01-30 23:20:31 +0200

    Added some console interactions

--- src/main/java/qa/automation/homework1.java
+++ src/main/java/qa/automation/homework1.java
@@ -1,4 +1,25 @@
 package qa.automation;
 
+import java.util.Scanner;
+
 public class homework1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Please enter your first name");
+
+        Scanner scanner = new Scanner (System.in);
+
+        String firstName = scanner.next();
+
+        System.out.println("Please enter your last name");
+
+        String lastName = scanner.next();
+
+        System.out.println("Enter your favorite hobby");
+
+        String hobby = scanner.next();
+
+        System.out.println("Your firstName is " + firstName + " and your last name is " + lastName + " and your favorite hobby is " + hobby);
+    }
 }
